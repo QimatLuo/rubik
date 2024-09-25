cfc9079f67fa2166588581cce68330a03c55b351
Qimat Luo <qimatluo@westpharma.com>
2024-09-25 22:32:37 +0800

    Apply style

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,13 +1,17 @@
 import "./style.css";
-import { map, merge, scan, startWith, withLatestFrom } from "rxjs";
+import { expand, map, merge, of, take, withLatestFrom } from "rxjs";
 import { random, verify } from "@rubik/core";
 import { keyup$ } from "./feature/keyboard";
 import { click$ } from "./feature/click";
 import { fromElementIdEvent, getElementById } from "./rxjs";
 
-const changed$ = merge(keyup$, click$).pipe(
-  startWith((x) => x),
-  scan((s, f) => f(s), random()),
+const changed$ = of(random()).pipe(
+  expand((raw) =>
+    merge(keyup$, click$).pipe(
+      map((f) => f(raw)),
+      take(1),
+    ),
+  ),
 );
 const temp$ = changed$.pipe(map(([x]) => x));
 const slots$ = changed$.pipe(map(([, ...xs]) => xs));
